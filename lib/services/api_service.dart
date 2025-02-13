ba52f3d51172ca222c9d96b78da1f15b818957c6
KC Ukeka <ebubeukeka@gmail.com>
2025-02-13 11:57:47 -0600

    added comments to basurl

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:waitingboard/model/provider_info.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://127.0.0.1:5000'; // Change to IP address for device testing
+  static const String baseUrl = 'http://127.0.0.1:5000'; // Change to IP address for device testing, currently set ti localhost ip port 500
 
 
 
