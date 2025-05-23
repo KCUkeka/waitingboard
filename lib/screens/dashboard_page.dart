8a33ae97e2b1d01157550c9693a360b79b4c67fb
KC Ukeka <ebubeukeka.gmail.com>
2025-05-23 12:11:42 -0500

    edited the name for anc services

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -230,7 +230,7 @@ class _DashboardPageState extends State<DashboardPage> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
-                                      p.dashboardName,
+                                      p.dashboardName.split(',').first.trim(),
                                       style: TextStyle(
                                           fontSize: 16,
                                           fontWeight: FontWeight.bold),
